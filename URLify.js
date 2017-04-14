7daf5d571301df006cd66302c8ba912084bd6ec5
Dennis Nguyen <nguyendennist97@Gmail.com>
2017-04-13 22:06:23 -0700

    Implemented URLify.

---
+++ URLify.js
@@ -0,0 +1,11 @@
+/* 
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space
+at the end to hod the additional characters, and that you are given the "true" length of the string. (Note: If
+implementing in Java, please use a character array so that you can perform this operation in place.)
+
+Example
+Input: "Mr John Smith     ", 13
+Output: "Mr%20JohnSmith"
+*/
+
+let URLify = (str, strLen) => str.substring(0, strLen).replace(/\s/g, '%20')
