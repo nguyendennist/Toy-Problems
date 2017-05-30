b6894bce1dc3b16c04f54ce0bc67724e1763b04e
Dennis Nguyen <nguyendennist97@Gmail.com>
2017-05-30 11:09:05 -0700

    Implemented removeDups

---
+++ Remove Dups.js
@@ -0,0 +1,70 @@
+/*
+Write code to remove duplicates from an unsorted linkedin list.
+
+Examples:
+Input: 
+{
+  value: 5,
+  next: {
+    value: 10,
+    next: {
+      value: 15,
+      next: null
+    }
+  }
+}
+
+Output: 
+{
+  value: 5,
+  next: {
+    value: 10,
+    next: {
+      value: 15,
+      next: null
+    }
+  }
+}
+
+Input: 
+{
+  value: 5,
+  next: {
+    value: 5,
+    next: {
+      value: 15,
+      next: null
+    }
+  }
+}
+
+Output:
+{
+  value: 5,
+  next: {
+    value: 15,
+    next: null
+  }
+}
+*/
+
+let removeDups = (linkedList) => {
+  let copy = JSON.parse(JSON.stringify(linkedList));
+  let copyValues = {};
+  let copyFirstValue = JSON.stringify(copy.value);
+  copyValues[copyFirstValue] = true;
+  let pointer = copy;
+
+  while (pointer.next) {
+    let stringValue = JSON.stringify(pointer.next.value);
+
+    if (copyValues[stringValue]) {
+      pointer.next = pointer.next.next;
+    } else {
+      copyValues[stringValue] = true;
+      pointer = pointer.next;
+    }
+  }
+
+  return copy;
+};
